baa1f0f71e1d31d303998e9bfd28d1285cf965b6
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-08-16 11:49:33 +0300

    feat: add billing and shipping address fields in registration

---
+++ src/constants/errorMassages.ts
@@ -0,0 +1,6 @@
+const REQUIRED_FILL = 'This field must be filled';
+const MIN_1_LENGTH = 'This field must be at least 1 character long';
+const MIN_8_LENGTH = 'This field must be at least 8 character long';
+const ONLY_LETTERS = 'This field must contain only letters';
+
+export { REQUIRED_FILL, MIN_1_LENGTH, MIN_8_LENGTH, ONLY_LETTERS };
