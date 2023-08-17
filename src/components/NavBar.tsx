0f54aa4b572127af70c627610012afc9df0dc12a
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-08-18 00:51:12 +0300

    feat(project): implement user store with redirection

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,44 @@
-import { NavLink } from 'react-router-dom';
-import { AUTH_ROUTE, DEFAULT_ROUTE, REGISTRATION_ROUTE } from '../constants/route';
+import { NavLink, useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../store/Context';
+import { AUTH_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../constants/route';
 
-const NavBar = () => {
+const NavBar = observer(() => {
+  const { user } = useContext(Context);
+  const location = useLocation();
+  const isLoginPage = location.pathname === AUTH_ROUTE;
+  const isRegistrationPage = location.pathname === REGISTRATION_ROUTE;
+
   return (
-    <div>
-      <NavLink to={DEFAULT_ROUTE}>
-        <button>Main page</button>
-      </NavLink>
-      <NavLink to={REGISTRATION_ROUTE}>
-        <button>Registration page</button>
-      </NavLink>
-      <NavLink to={AUTH_ROUTE}>
-        <button>Auth page</button>
-      </NavLink>
-    </div>
+    <nav>
+      {user?.isAuth && (
+        <div>
+          <NavLink to={MAIN_ROUTE}>
+            <button>Main page</button>
+          </NavLink>
+          <button onClick={() => user.setIsAuth(false)}>LogOut</button>
+        </div>
+      )}
+      {!user?.isAuth && (
+        <div>
+          <NavLink to={MAIN_ROUTE}>
+            <button>Main page</button>
+          </NavLink>
+          {!isRegistrationPage && (
+            <NavLink to={REGISTRATION_ROUTE}>
+              <button>Registration page</button>
+            </NavLink>
+          )}
+          {!isLoginPage && (
+            <NavLink to={AUTH_ROUTE}>
+              <button>Auth page</button>
+            </NavLink>
+          )}
+        </div>
+      )}
+    </nav>
   );
-};
+});
 
 export { NavBar };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { App } from './App';
+import { Context } from './store/Context';
+import { UserStore } from './store/UserStore';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Context.Provider
+      value={{
+        user: new UserStore(),
+      }}
+    >
+      <App />
+    </Context.Provider>
   </React.StrictMode>
 );
