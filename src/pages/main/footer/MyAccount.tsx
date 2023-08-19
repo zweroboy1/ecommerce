52fe2505fbb518fd7a893b2587f12ee60bbb3606
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-08-19 21:24:33 +0300

    feat: add banner carousel

--- src/pages/main/footer/MyAccount.tsx
+++ src/pages/main/footer/MyAccount.tsx
@@ -1,16 +1,16 @@
 import FooterMenu from './FooterMenu';
 import { SocialLinks } from './SocialLinks';
-import { AUTH_ROUTE, REGISTRATION_ROUTE } from '../../../constants/route';
+import { ABOUT_ROUTE, CONTACTS_ROUTE } from '../../../constants/route';
 
 const MyAccount = () => {
   const menuItems = [
-    { text: 'Войти', link: AUTH_ROUTE, rel: 'nofollow' },
-    { text: 'Создать учетную запись', link: REGISTRATION_ROUTE, rel: 'nofollow' },
+    { text: 'О нас', link: ABOUT_ROUTE },
+    { text: 'Обратная связь', link: CONTACTS_ROUTE },
   ];
 
   return (
     <div>
-      <FooterMenu className="my-account" title="Моя учетная запись" menuItems={menuItems} />
+      <FooterMenu className="my-account" title="Информация" menuItems={menuItems} />
       <SocialLinks />
     </div>
   );
