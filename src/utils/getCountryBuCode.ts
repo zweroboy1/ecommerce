f4b10201886baf26b937caf03f0ec4a9f955fe92
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-08-24 17:37:15 +0300

    Revert "feat(user account): add basic layout to user profile with user data"
    This reverts commit 1e83b66392d11d8124980356c3b79efec26f142b.

--- src/utils/getCountryBuCode.ts
+++
@@ -1,8 +0,0 @@
-import { country } from '../constants/country';
-
-const getCountryByCode = (code: string): string =>
-  Object.values(country).includes(code)
-    ? Object.entries(country).filter(([, value]) => code === value)[0][0]
-    : code;
-
-export { getCountryByCode };
