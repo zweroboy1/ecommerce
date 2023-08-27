974fb41405214550d1d567b4c28acad1dff87645
Konrad <fprognoz@gmail.com>
2023-08-27 13:48:54 +0300

    fix(registration): spaces and some other symbols are allowed for input

--- src/utils/registrationValidation.ts
+++ src/utils/registrationValidation.ts
@@ -60,14 +60,14 @@ const stringValidation = yup
   .trim()
   .required(REQUIRED_FILL)
   .min(1, MIN_1_LENGTH)
-  .matches(/^[a-zA-Zа-яА-ЯъЪЇїЫы ]+$/, ONLY_LETTERS);
+  .matches(/^[a-zA-Zа-яА-ЯъЪЇїЫыЁё \-`]+$/, ONLY_LETTERS);
 
 const cityValidation = yup
   .string()
   .trim()
   .required(REQUIRED_FILL)
   .min(1, MIN_1_LENGTH)
-  .matches(/^[a-zA-Zа-яА-ЯъЪЇїЫы -]+$/, ONLY_LETTERS);
+  .matches(/^[a-zA-Zа-яА-ЯъЪЇїЫыЁё \-`]+$/, ONLY_LETTERS);
 
 const billingPostCodeValidation = yup
   .string()
@@ -166,7 +166,7 @@ const streetValidation = yup
   .trim()
   .required(REQUIRED_FILL)
   .min(1, MIN_1_LENGTH)
-  .matches(/[a-zA-Zа-яА-ЯъЪЇїЫы -]/, ONE_LETTER);
+  .matches(/[a-zA-Zа-яА-ЯъЪЇїЫыЁё \-`]/, ONE_LETTER);
 
 const dateOfBirthValidation = yup
   .date()
