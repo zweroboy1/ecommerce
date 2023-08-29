a6de88ac3a3beb13d94714533ab033644131bf60
Konrad <fprognoz@gmail.com>
2023-08-29 18:15:00 +0300

    feat(catalog): add pagination in catalog
    n

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -8,14 +8,15 @@ interface ProductListProps {
 
 // Моковая функция getProducts
 function getProducts(): Product[] {
-  // Здесь вы можете вернуть какие-то моковые продукты для данной категории
-  // В данном случае просто генерируем 4 моковых продукта
-  // console.log(category);
   return [
     { id: 1, name: 'Product 1', price: 100 },
     { id: 2, name: 'Product 2', price: 200 },
     { id: 3, name: 'Product 3', price: 150 },
     { id: 4, name: 'Product 4', price: 300 },
+    { id: 5, name: 'Product 5', price: 120 },
+    { id: 6, name: 'Product 6', price: 130 },
+    { id: 7, name: 'Product 7', price: 220 },
+    { id: 8, name: 'Product 8', price: 500 },
   ];
 }
 
