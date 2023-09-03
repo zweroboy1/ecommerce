5aae5d0bd51e025cc5a1d9715b5bc73d9e9ca93a
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-03 04:30:15 +0300

    feat: add adaptive design

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { Product } from '../types';
 import { getProducts } from '../services/commercetoolsApi';
 import { mapProduct } from '../utils/mapProduct';
+import { formatPrice } from '../utils/formatPrice';
 
 interface ProductListProps {
   category: string;
@@ -59,6 +60,8 @@ function sortProducts(products: Product[], sortOption: string): Product[] {
 
 function ProductList({ category, currentSort }: ProductListProps) {
   const [products, setProducts] = useState<Product[]>([]);
+  const [expandedDescription, setExpandedDescription] = useState<string | null>(null);
+  const [isHoveredMap, setIsHoveredMap] = useState<{ [key: string]: boolean }>({});
 
   useEffect(() => {
     async function getProductsFromServer() {
@@ -81,11 +84,23 @@ function ProductList({ category, currentSort }: ProductListProps) {
     <div className="goods">
       {products.map((product) => (
         <div key={product.id} className="goods__card">
-          <div className="goods__image">
+          <div
+            className="goods__image"
+            onMouseEnter={() => setIsHoveredMap({ ...isHoveredMap, [product.id]: true })}
+            onMouseLeave={() => setIsHoveredMap({ ...isHoveredMap, [product.id]: false })}
+          >
             <NavLink to={`/product/${product.slug}`}>
-              <img src={product.images[0]} alt={product.name} />
+              <img
+                src={
+                  isHoveredMap[product.id] && product.images[1]
+                    ? product.images[1]
+                    : product.images[0]
+                }
+                alt={product.name}
+              />
             </NavLink>
           </div>
+
           <div className="goods__info">
             <h2 className="goods__name">
               <NavLink className="goods__title" to={`/product/${product.slug}`}>
@@ -95,17 +110,37 @@ function ProductList({ category, currentSort }: ProductListProps) {
             <div className="goods__prices">
               <div className="goods__price-block">
                 <span className="goods__discounted-price">
-                  UAH {(product.price / 100).toFixed(2)}
+                  UAH {formatPrice(product.price / 100)}
                 </span>
                 <span className="goods__old-price">
-                  UAH {(product.price / 100 + 100).toFixed(2)}
+                  UAH {formatPrice(product.price / 100 + 100)}
                 </span>
               </div>
               <div className="goods__control">
                 <i className="goods__control-icon cart__icon header-icon"></i>
               </div>
             </div>
-            <div>{product.description}</div>
+            <div className="goods__description">
+              {expandedDescription === product.id ? (
+                product.description
+              ) : (
+                <>
+                  {product.description.length > 70 ? (
+                    <>
+                      {product.description.substring(0, 70)}...
+                      <button
+                        className="goods__expand-description button button-second"
+                        onClick={() => setExpandedDescription(product.id)}
+                      >
+                        Подробнее
+                      </button>
+                    </>
+                  ) : (
+                    product.description
+                  )}
+                </>
+              )}
+            </div>
           </div>
         </div>
       ))}
--- src/pages/product/ProductImages.tsx
+++ src/pages/product/ProductImages.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import { ProductImagesProps } from '../../types';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { ModalImage } from './ModalImage';
 
 const ProductImages: React.FC<ProductImagesProps> = ({ productImages }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openModal = (image: string) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage('');
+    setIsModalOpen(false);
+  };
+
   return (
-    <Carousel>
-      {productImages.map((image, index) => (
-        <div key={index} className="product__image">
-          <img key={index} src={image} alt={`Product ${index}`} />
-        </div>
-      ))}
-    </Carousel>
+    <div>
+      <Carousel>
+        {productImages.map((image, index) => (
+          <div key={index} className="product__image" onClick={() => openModal(image)}>
+            <img key={index} src={image} alt={`Product ${index}`} />
+          </div>
+        ))}
+      </Carousel>
+      {isModalOpen && <ModalImage imageUrl={selectedImage} onClose={closeModal} />}
+    </div>
   );
 };
 
