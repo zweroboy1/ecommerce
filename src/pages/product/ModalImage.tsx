81a4fdd6078034642871ff669e75f73982772d7b
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-03 19:00:37 +0300

    feat: add account style

--- src/pages/product/ModalImage.tsx
+++ src/pages/product/ModalImage.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import { useRef } from 'react';
+import { Carousel } from 'react-responsive-carousel';
 
 interface ModalImageProps {
-  imageUrl: string;
+  images: string[];
+  selectedImageIndex: number;
   onClose: () => void;
 }
 
-const ModalImage: React.FC<ModalImageProps> = ({ imageUrl, onClose }) => {
+const ModalImage: React.FC<ModalImageProps> = ({ images, selectedImageIndex, onClose }) => {
+  const carouselRef = useRef(null);
+
   return (
     <div className="modal" onClick={onClose}>
-      <div className="modal-content">
-        <img src={imageUrl} alt="Product" />
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <span className="close-button" onClick={onClose}>
           &times;
         </span>
+        <Carousel selectedItem={selectedImageIndex} ref={carouselRef}>
+          {images.map((imageUrl, index) => (
+            <div key={index} className="carousel-image">
+              <img src={imageUrl} alt={`Product ${index}`} />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
