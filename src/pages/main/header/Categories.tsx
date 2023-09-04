58c88ad6598802ac6370d3640467d2c77cb76458
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-04 17:18:55 +0300

    fix: fix style catalog

--- src/pages/main/header/Categories.tsx
+++ src/pages/main/header/Categories.tsx
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { CatalogMenu } from '../../../components/CatalogMenu';
 import { CATEGORIES } from '../../../constants/categories';
 
 const Categories = () => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const closeMenu = () => {
+    setMenuVisible(false);
+  };
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
 
-  // const closeMenu = () => {
-  //   setMenuVisible(false);
-  // };
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    toggleMenu();
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuVisible && menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [menuVisible]);
 
   return (
     <div className="menu">
-      <div className="menu__title" onClick={toggleMenu}>
+      <div className="menu__title" onClick={handleClick}>
         <i className="menu__icon"></i>
         <span>Категории</span>
       </div>
-      <div className={`menu__categories ${menuVisible ? 'active' : ''}`}>
-        <CatalogMenu categories={CATEGORIES} />
+      <div className={`menu__categories ${menuVisible ? 'active' : ''}`} ref={menuRef}>
+        <CatalogMenu categories={CATEGORIES} closeMenu={closeMenu} />
       </div>
     </div>
   );
