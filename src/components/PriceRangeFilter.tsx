39626ab0926483c063aac633888f4983a2d768e5
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-04 06:18:25 +0300

    feat: add style filter

--- src/components/PriceRangeFilter.tsx
+++ src/components/PriceRangeFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
@@ -11,6 +11,26 @@ const PriceRangeFilter: React.FC<{
   const [maxPrice, setMaxPrice] = useState<number>(MAX_PRICE_FILTER);
 
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [expanded, setExpanded] = useState(false);
+  const filtersRef = useRef<HTMLDivElement | null>(null);
+
+  const handleToggleFilter = () => {
+    setExpanded(!expanded);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (filtersRef.current && !filtersRef.current.contains(event.target as Node)) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
 
   const handleSliderChange = (values: number[]) => {
     setMinPrice(values[0]);
@@ -25,25 +45,88 @@ const PriceRangeFilter: React.FC<{
     }, 300);
   };
 
+  const handleMinInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value, 10);
+    if (!Number.isNaN(newValue)) {
+      setMinPrice(newValue);
+    }
+  };
+
+  const handleMaxInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value, 10);
+    if (!Number.isNaN(newValue)) {
+      setMaxPrice(newValue);
+    }
+  };
+
+  const handleResetFilter = () => {
+    setMinPrice(0);
+    setMaxPrice(MAX_PRICE_FILTER);
+    onPriceChange(0, MAX_PRICE_FILTER);
+  };
+
   return (
-    <div>
-      <label>Выберите диапазон цен:</label>
-      <Slider
-        range
-        min={0}
-        max={MAX_PRICE_FILTER}
-        step={100}
-        value={[minPrice, maxPrice]}
-        onChange={(values: number | number[]) => {
-          if (Array.isArray(values)) {
-            const [newMinValue, newMaxValue] = values;
-            handleSliderChange([newMinValue, newMaxValue]);
-          }
-        }}
-      />
-      <div>
-        <span>Мин. цена: {minPrice}</span>
-        <span>Макс. цена: {maxPrice}</span>
+    <div className={`filters__item ${expanded ? 'open' : ''}`} ref={filtersRef}>
+      <div className="filters__header" onClick={handleToggleFilter}>
+        Цена
+        <i className={`filters__icon ${expanded ? 'arrow-up' : 'arrow-down'}`}></i>
+      </div>
+      <div className={`filters__content ${expanded ? '' : 'hidden'}`}>
+        <div className="filters__price-slider">
+          <div className="filters__price-inputs">
+            <bdi>
+              <span className="filters__price-prefix">UAH</span>
+              <input
+                type="number"
+                min={0}
+                max={MAX_PRICE_FILTER}
+                value={minPrice}
+                onChange={handleMinInputChange}
+              />
+            </bdi>
+            <span>-</span>
+            <bdi>
+              <span className="filters__price-suffix">UAH</span>
+              <input
+                type="number"
+                min={0}
+                max={MAX_PRICE_FILTER}
+                value={maxPrice}
+                onChange={handleMaxInputChange}
+              />
+            </bdi>
+          </div>
+          <Slider
+            range
+            min={0}
+            max={MAX_PRICE_FILTER}
+            step={100}
+            value={[minPrice, maxPrice]}
+            onChange={(values: number | number[]) => {
+              if (Array.isArray(values)) {
+                const [newMinValue, newMaxValue] = values;
+                handleSliderChange([newMinValue, newMaxValue]);
+              }
+            }}
+          />
+          <div className="filters__price-labels">
+            <span>UAH {minPrice}</span>
+            <span>UAH {maxPrice}</span>
+          </div>
+        </div>
+        <div className="filters__reset-block">
+          <a className="filters__reset reset" onClick={handleResetFilter}>
+            Сбросить
+            <i className="reset__icon"></i>
+          </a>
+        </div>
+        <div
+          className={`filters__close-button close ${expanded ? '' : 'hidden'}`}
+          onClick={handleToggleFilter}
+        >
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
