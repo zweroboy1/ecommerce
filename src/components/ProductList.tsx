b1b8229e4c0b45839b1a585cf928d9ede6cbbc76
Konrad <fprognoz@gmail.com>
2023-09-06 19:02:58 +0300

    fix(catalog): fix styles for interactive card
    #75

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,76 +1,59 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Product } from '../types';
 import { formatPrice } from '../utils/formatPrice';
 
 function ProductList({ products }: { products: Product[] }) {
-  const [isHoveredMap, setIsHoveredMap] = useState<{ [key: string]: boolean }>({});
-  const [expandedDescription, setExpandedDescription] = useState<string | null>(null);
   return (
     <div className="goods">
       {products.map((product) => (
         <div key={product.id} className="goods__card">
-          <div
-            className="goods__image"
-            onMouseEnter={() => setIsHoveredMap({ ...isHoveredMap, [product.id]: true })}
-            onMouseLeave={() => setIsHoveredMap({ ...isHoveredMap, [product.id]: false })}
-          >
-            <NavLink to={`/product/${product.slug}`}>
-              <img
-                src={
-                  isHoveredMap[product.id] && product.images[1]
-                    ? product.images[1]
-                    : product.images[0]
-                }
-                alt={product.name}
-              />
-            </NavLink>
-          </div>
-
-          <div className="goods__info">
-            <h2 className="goods__name">
-              <NavLink className="goods__title" to={`/product/${product.slug}`}>
-                {product.name}
-              </NavLink>
-            </h2>
-            <div className="goods__prices">
-              <div className="goods__price-block">
-                {product.discountedPrice ? (
+          <div className="goods__wrapper">
+            <div className="goods__image">
+              <NavLink to={`/product/${product.slug}`}>
+                {product.images[1] ? (
                   <>
-                    <span className="goods__old-price">{formatPrice(product.price / 100)} ₴</span>
-
-                    <span className="goods__discounted-price">
-                      {formatPrice(product.discountedPrice / 100)} ₴
-                    </span>
+                    <img
+                      className="goods__image_first"
+                      src={product.images[0]}
+                      alt={`${product.name} - image #1`}
+                    />
+                    <img
+                      className="goods__image_second"
+                      src={product.images[1]}
+                      alt={`${product.name} - image #2`}
+                    />
                   </>
                 ) : (
-                  <span className="goods__price">{formatPrice(product.price / 100)} ₴</span>
+                  <img className="goods__image_only" src={product.images[0]} alt={product.name} />
                 )}
-              </div>
-              <div className="goods__control">
-                <i className="goods__control-icon cart__icon header-icon"></i>
-              </div>
+              </NavLink>
             </div>
-            <div className="goods__description">
-              {expandedDescription === product.id ? (
-                product.description
-              ) : (
-                <>
-                  {product.description.length > 70 ? (
+
+            <div className="goods__info">
+              <h2 className="goods__name">
+                <NavLink className="goods__title" to={`/product/${product.slug}`}>
+                  {product.name}
+                </NavLink>
+              </h2>
+              <div className="goods__prices">
+                <div className="goods__price-block">
+                  {product.discountedPrice ? (
                     <>
-                      {product.description.substring(0, 70)}...
-                      <button
-                        className="goods__expand-description button button-second"
-                        onClick={() => setExpandedDescription(product.id)}
-                      >
-                        Подробнее
-                      </button>
+                      <span className="goods__old-price">{formatPrice(product.price / 100)} ₴</span>
+
+                      <span className="goods__discounted-price">
+                        {formatPrice(product.discountedPrice / 100)} ₴
+                      </span>
                     </>
                   ) : (
-                    product.description
+                    <span className="goods__price">{formatPrice(product.price / 100)} ₴</span>
                   )}
-                </>
-              )}
+                </div>
+                <div className="goods__control">
+                  <i className="goods__control-icon cart__icon header-icon"></i>
+                </div>
+              </div>
+              <div className="goods__description1">{product.description}</div>
             </div>
           </div>
         </div>
