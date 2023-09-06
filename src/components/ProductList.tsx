fcc5d18723cb588f14aa5a2475287b8f999761ba
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-06 12:08:22 +0300

    fix: fix button details

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -38,62 +38,60 @@ function ProductList({ products }: { products: Product[] }) {
           onMouseEnter={() => handleCardMouseEnter(product.id)}
           onMouseLeave={handleCardMouseLeave}
         >
-          <div className="goods__wrapper">
-            <div
-              className="goods__image"
-              onMouseEnter={() => handleImageMouseEnter(product.id)}
-              onMouseLeave={() => handleImageMouseLeave(product.id)}
-            >
-              <NavLink to={`/product/${product.slug}`}>
-                <img
-                  src={
-                    isHoveredMap[product.id] && product.images[1]
-                      ? product.images[1]
-                      : product.images[0]
-                  }
-                  alt={product.name}
-                />
+          <div
+            className="goods__image"
+            onMouseEnter={() => handleImageMouseEnter(product.id)}
+            onMouseLeave={() => handleImageMouseLeave(product.id)}
+          >
+            <NavLink to={`/product/${product.slug}`}>
+              <img
+                src={
+                  isHoveredMap[product.id] && product.images[1]
+                    ? product.images[1]
+                    : product.images[0]
+                }
+                alt={product.name}
+              />
+            </NavLink>
+          </div>
+
+          <div className="goods__info">
+            <h2 className="goods__name">
+              <NavLink className="goods__title" to={`/product/${product.slug}`}>
+                {product.name}
               </NavLink>
-            </div>
+            </h2>
+            <div className="goods__prices">
+              <div className="goods__price-block">
+                {product.discountedPrice ? (
+                  <>
+                    <span className="goods__old-price">{formatPrice(product.price / 100)} ₴</span>
 
-            <div className="goods__info">
-              <h2 className="goods__name">
-                <NavLink className="goods__title" to={`/product/${product.slug}`}>
-                  {product.name}
-                </NavLink>
-              </h2>
-              <div className="goods__prices">
-                <div className="goods__price-block">
-                  {product.discountedPrice ? (
-                    <>
-                      <span className="goods__old-price">{formatPrice(product.price / 100)} ₴</span>
-
-                      <span className="goods__discounted-price">
-                        {formatPrice(product.discountedPrice / 100)} ₴
-                      </span>
-                    </>
-                  ) : (
-                    <span className="goods__price">{formatPrice(product.price / 100)} ₴</span>
-                  )}
-                </div>
-                <div className="goods__control">
-                  <i className="goods__control-icon cart__icon header-icon"></i>
-                </div>
+                    <span className="goods__discounted-price">
+                      {formatPrice(product.discountedPrice / 100)} ₴
+                    </span>
+                  </>
+                ) : (
+                  <span className="goods__price">{formatPrice(product.price / 100)} ₴</span>
+                )}
               </div>
-              <div className="goods__description">
-                <div className="goods__description1">
-                  {expandedDescription === product.id ? (
-                    product.description
-                  ) : (
-                    <>
-                      {product.description.length > 70 ? (
-                        <>{product.description.substring(0, 70)}...</>
-                      ) : (
-                        product.description
-                      )}
-                    </>
-                  )}
-                </div>
+              <div className="goods__control">
+                <i className="goods__control-icon cart__icon header-icon"></i>
+              </div>
+            </div>
+            <div className="goods__description">
+              <div className="goods__description-txt">
+                {expandedDescription === product.id ? (
+                  product.description
+                ) : (
+                  <>
+                    {product.description.length > 70 ? (
+                      <>{product.description.substring(0, 70)}...</>
+                    ) : (
+                      product.description
+                    )}
+                  </>
+                )}
               </div>
             </div>
           </div>
