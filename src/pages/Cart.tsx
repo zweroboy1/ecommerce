4dea1c8b47bb3df93932cecaacf9c6a2af829ac3
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-18 03:29:01 +0300

    fix: fix style empty cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -204,21 +204,21 @@ const Cart = observer(() => {
                 <div className="cart__form-wrapper">
                   <div className="cart__form">
                     <h1 className="cart__title-page">Корзина</h1>
-                    <div className="cart__buttons-container">
-                      <div className="cart__left-buttons">
-                        <NavLink to={CATALOG_ROUTE} className="button button-second">
-                          <bdi>Продолжить покупки</bdi>
-                        </NavLink>
-                      </div>
-                      {userCart && userCart.lineItems.length > 0 && (
+                    {userCart && userCart.lineItems.length > 0 && (
+                      <div className="cart__buttons-container">
+                        <div className="cart__left-buttons">
+                          <NavLink to={CATALOG_ROUTE} className="button button-second">
+                            <bdi>Продолжить покупки</bdi>
+                          </NavLink>
+                        </div>
                         <div className="cart__right-buttons">
                           <a role="button" className="button">
                             <span className="cart__icon-ok"></span>
                             <bdi>Оформить заказ</bdi>
                           </a>
                         </div>
-                      )}
-                    </div>
+                      </div>
+                    )}
 
                     <div>
                       <div className="cart__content">
@@ -249,11 +249,9 @@ const Cart = observer(() => {
                           </div>
                         ) : (
                           <div className="cart__empty">
-                            <span>
-                              Ваша корзина пока еще пуста...Мы рады предложить Вам ознакомиться с
-                              нашим
-                            </span>
-                            <NavLink to={CATALOG_ROUTE} className="button button-second">
+                            <span>Ваша корзина пока еще пуста...</span>
+                            <span>Мы рады предложить Вам ознакомиться с нашим</span>
+                            <NavLink to={CATALOG_ROUTE} className="button">
                               Каталогом товаров
                             </NavLink>
                           </div>
@@ -261,89 +259,93 @@ const Cart = observer(() => {
                       </div>
                     </div>
 
-                    <div className="cart__total">
-                      <div className="cart__total-wrapper">
-                        <div className="cart__coupons">
-                          <div>
-                            <div className="cart__processed-form">
-                              <div className="cart__hint">
-                                <label className="cart__hint-label">Промо-код</label>
-                                <input
-                                  type="text"
-                                  className="cart__hint-text"
-                                  value={promoCode}
-                                  onChange={handleInputChange}
-                                  onKeyDown={handleKeyDown}
-                                />
-                                <button
-                                  title="Применить"
-                                  className="cart__btn-go"
-                                  type="button"
-                                  onClick={applyCode}
-                                >
-                                  Применить
-                                </button>
+                    {userCart?.lineItems && userCart.lineItems.length > 0 && (
+                      <div className="cart__total">
+                        <div className="cart__total-wrapper">
+                          <div className="cart__coupons">
+                            <div>
+                              <div className="cart__processed-form">
+                                <div className="cart__hint">
+                                  <label className="cart__hint-label">Промо-код</label>
+                                  <input
+                                    type="text"
+                                    className="cart__hint-text"
+                                    value={promoCode}
+                                    onChange={handleInputChange}
+                                    onKeyDown={handleKeyDown}
+                                  />
+                                  <button
+                                    title="Применить"
+                                    className="cart__btn-go"
+                                    type="button"
+                                    onClick={applyCode}
+                                  >
+                                    Применить
+                                  </button>
+                                </div>
                               </div>
                             </div>
+
+                            <ul className="cart__discount-info">
+                              <li className="cart__discount-list">
+                                <span className="caret-outer"></span>
+                                <span className="caret-inner"></span>
+                              </li>
+                              <li className="cart__coupons-item">
+                                <div className="cart__promotions">
+                                  <span className="cart__bonus">Ваши бонусы</span>
+                                  <ul>
+                                    <li>
+                                      <a className="cart__dashed-link">Скидка 5%</a>
+                                      <div className="cart__wysiwyg-content">
+                                        <p>
+                                          При покупке комплекта Roborock Vacuum Cleaner S7 вы
+                                          получите скидку 5%.
+                                        </p>
+                                      </div>
+                                    </li>
+                                  </ul>
+                                </div>
+                              </li>
+                            </ul>
                           </div>
 
-                          <ul className="cart__discount-info">
-                            <li className="cart__discount-list">
-                              <span className="caret-outer"></span>
-                              <span className="caret-inner"></span>
+                          <ul className="cart__statistic-list">
+                            <li className="cart__statistic-item">
+                              <span className="cart__statistic-title">
+                                Сумма без промокодов и акций
+                              </span>
+                              <span className="cart__statistic-value">
+                                <bdi>
+                                  <span>{formatPrice(totalSum / 100)} ₴</span>
+                                </bdi>
+                              </span>
                             </li>
-                            <li className="cart__coupons-item">
-                              <div className="cart__promotions">
-                                <span className="cart__bonus">Ваши бонусы</span>
-                                <ul>
-                                  <li>
-                                    <a className="cart__dashed-link">Скидка 5%</a>
-                                    <div className="cart__wysiwyg-content">
-                                      <p>
-                                        При покупке комплекта Roborock Vacuum Cleaner S7 вы получите
-                                        скидку 5%.
-                                      </p>
-                                    </div>
-                                  </li>
-                                </ul>
-                              </div>
+                            <li className="cart__statistic-item discount">
+                              <span className="cart__statistic-title">Итоговая скидка</span>
+                              <span className="cart__statistic-value">
+                                <bdi>
+                                  <span>{formatPrice((totalSum - totalAmount) / 100)} ₴</span>
+                                </bdi>
+                              </span>
+                            </li>
+                          </ul>
+                          <ul className="cart__statistic-total-list">
+                            <li className="cart__statistic-total-item">
+                              <span className="cart__statistic-total-title">
+                                Итоговая стоимость
+                              </span>
+                              <span className="cart__statistic-total-value">
+                                <bdi>
+                                  <span>{formatPrice(totalAmount / 100)}</span>
+                                  <span> ₴</span>
+                                </bdi>
+                              </span>
                             </li>
                           </ul>
                         </div>
-
-                        <ul className="cart__statistic-list">
-                          <li className="cart__statistic-item">
-                            <span className="cart__statistic-title">
-                              Сумма без промокодов и акций
-                            </span>
-                            <span className="cart__statistic-value">
-                              <bdi>
-                                <span>{formatPrice(totalSum / 100)} ₴</span>
-                              </bdi>
-                            </span>
-                          </li>
-                          <li className="cart__statistic-item discount">
-                            <span className="cart__statistic-title">Итоговая скидка</span>
-                            <span className="cart__statistic-value">
-                              <bdi>
-                                <span>{formatPrice((totalSum - totalAmount) / 100)} ₴</span>
-                              </bdi>
-                            </span>
-                          </li>
-                        </ul>
-                        <ul className="cart__statistic-total-list">
-                          <li className="cart__statistic-total-item">
-                            <span className="cart__statistic-total-title">Итоговая стоимость</span>
-                            <span className="cart__statistic-total-value">
-                              <bdi>
-                                <span>{formatPrice(totalAmount / 100)}</span>
-                                <span> ₴</span>
-                              </bdi>
-                            </span>
-                          </li>
-                        </ul>
                       </div>
-                    </div>
+                    )}
 
                     <div className="cart__buttons-container">
                       <div className="cart__left-buttons">
