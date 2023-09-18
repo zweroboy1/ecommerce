8891e071504f59d4e360887b2a38442f1dd9e082
Konrad <fprognoz@gmail.com>
2023-09-18 22:20:15 +0300

    feat(cart): display applied promocodes in the footer of the cart

---
+++ src/constants/promocodes.ts
@@ -0,0 +1,23 @@
+const PROMOCODES = [
+  {
+    id: 'c66a586a-d934-4190-a027-f18e93e383bb',
+    code: 'minus1000',
+    description: '-1000₴ при сумме больше 10000₴',
+  },
+  {
+    id: 'bc635bf7-874f-4335-a049-83ab783cf912',
+    code: 'BLACK_FRIDAY',
+    description: '-10% от стоимости каждого товара',
+  },
+  {
+    id: '9d7a9415-80e2-4e5c-8a07-c92202a05af0',
+    code: '5percents',
+    description: '-5% от стоимости каждого товара',
+  },
+  {
+    id: '8fd9e978-0d18-45ea-b5f3-f01b3bc0ac65',
+    code: 'VIDEO_30',
+    description: '-30% от стоимости любой видеокамеры',
+  },
+];
+export { PROMOCODES };
