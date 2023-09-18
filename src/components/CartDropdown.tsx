d00c8af34daa3d87863cb0b04ee422f36f0d1cc3
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2023-09-18 23:11:55 +0300

    fix: empty cart dropdown

--- src/components/CartDropdown.tsx
+++ src/components/CartDropdown.tsx
@@ -7,77 +7,85 @@ interface CartDropdownProps {
 }
 const CartDropdown: React.FC<CartDropdownProps> = ({ closeCart, cartData }) => {
   const lineItems = cartData?.lineItems || [];
+  const cartIsEmpty = lineItems.length === 0;
 
   return (
     <div className="cart-dropdown">
       <div className="cart-dropdown__content">
         <div className="cart-dropdown__title">
-          Товары в корзине:
+          {cartIsEmpty ? 'Товаров нет!' : 'Товары в корзине:'}
           <div className="cart-dropdown__close" onClick={closeCart}>
             <i className="icon-close"></i>
           </div>
         </div>
-        <div className="cart-dropdown__product">
-          <ul className="cart-dropdown__list">
-            {lineItems.map((item) => (
-              <li key={item.id} className="cart-dropdown__item">
-                <div className="cart-dropdown__image">
-                  <img
-                    alt={item.name.ru}
-                    title={item.name.ru}
-                    src={item.variant.images[0].url}
-                    width="40"
-                    height="40"
-                  />
-                </div>
-                <div className="cart-dropdown__desc">
-                  <span className="cart-dropdown__name">{item.name.ru}</span>
-                  <p>
-                    <span>{item.quantity}</span>
-                    <span>&nbsp;x&nbsp;</span>
-                    <bdi>
-                      <span>
-                        {formatPrice(
-                          item.price.discounted
-                            ? item.price.discounted.value.centAmount / 100
-                            : item.price.value.centAmount / 100
-                        )}
-                      </span>
-                      <span> ₴</span>
-                    </bdi>
-                  </p>
-                </div>
-                <div className="cart-dropdown__tools"></div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="buttons-container">
-          <p className="buttons-container__subtotal">
-            Всего (без промокодов):&nbsp;
-            <span className="float-right">
-              {lineItems.reduce((total, item) => total + item.quantity, 0)}&nbsp;шт. на&nbsp;&nbsp;
-              <bdi>
-                <span className="cart-price">
-                  {formatPrice(
-                    lineItems.reduce((total, item) => {
-                      const price =
-                        item.price.discounted?.value.centAmount || item.price.value.centAmount;
-                      return total + (item.quantity * price) / 100;
-                    }, 0)
-                  )}
-                  ₴
-                </span>
-              </bdi>
-            </span>
-            <br />
-            &nbsp;
-          </p>
+        {cartIsEmpty ? (
+          <div className="cart-dropdown__empty">Корзина пуста</div>
+        ) : (
+          <div className="cart-dropdown__product">
+            <ul className="cart-dropdown__list">
+              {lineItems.map((item) => (
+                <li key={item.id} className="cart-dropdown__item">
+                  <div className="cart-dropdown__image">
+                    <img
+                      alt={item.name.ru}
+                      title={item.name.ru}
+                      src={item.variant.images[0].url}
+                      width="40"
+                      height="40"
+                    />
+                  </div>
+                  <div className="cart-dropdown__desc">
+                    <span className="cart-dropdown__name">{item.name.ru}</span>
+                    <p>
+                      <span>{item.quantity}</span>
+                      <span>&nbsp;x&nbsp;</span>
+                      <bdi>
+                        <span>
+                          {formatPrice(
+                            item.price.discounted
+                              ? item.price.discounted.value.centAmount / 100
+                              : item.price.value.centAmount / 100
+                          )}
+                        </span>
+                        <span> ₴</span>
+                      </bdi>
+                    </p>
+                  </div>
+                  <div className="cart-dropdown__tools"></div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {!cartIsEmpty && (
+          <div className="buttons-container">
+            <p className="buttons-container__subtotal">
+              Всего (без промокодов):&nbsp;
+              <span className="float-right">
+                {lineItems.reduce((total, item) => total + item.quantity, 0)}&nbsp;шт.
+                на&nbsp;&nbsp;
+                <bdi>
+                  <span className="cart-price">
+                    {formatPrice(
+                      lineItems.reduce((total, item) => {
+                        const price =
+                          item.price.discounted?.value.centAmount || item.price.value.centAmount;
+                        return total + (item.quantity * price) / 100;
+                      }, 0)
+                    )}
+                    ₴
+                  </span>
+                </bdi>
+              </span>
+              <br />
+              &nbsp;
+            </p>
 
-          <a href="/cart" rel="nofollow" className="button button-cart">
-            Перейти в Корзину
-          </a>
-        </div>
+            <a href="/cart" rel="nofollow" className="button button-cart">
+              Перейти в Корзину
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
