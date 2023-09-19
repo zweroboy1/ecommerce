32bba0dc6bd0e9003296c8f9918e4047ec232ac2
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-09-19 14:56:24 +0300

    feat(cart): add fetch cart in visit to cart page
    add fetch cart in visit to cart page

--- src/constants/apiMessages.ts
+++ src/constants/apiMessages.ts
@@ -6,6 +6,7 @@ const CT_INVALID_JSON_ERROR = 'Request body does not contain valid JSON.';
 const CT_EXISTING_CUSTOMER_ERROR = 'There is already an existing customer with the provided email.';
 const CT_NETWORK_PROBLEM =
   'К сожалению, не получается загрузить данные с сервера. Проверьте качество подключения!';
+const CT_NETWORK_OR_SERVER_PROBLEM = 'К сожалению, не получается загрузить данные с сервера.';
 const CT_PRODUCT_NOT_FOUND = 'Продукт с данным id не найден в нашей базе';
 const CT_PRODUCT_NO_ID = 'Неправильный идентификатор продукта';
 const CT_BAD_PROMOCODE = 'Данный промокод не действует!';
@@ -24,4 +25,5 @@ export {
   CT_BAD_PROMOCODE,
   CT_FAILED_TO_FETCH,
   CT_UNKNOWN_ERROR,
+  CT_NETWORK_OR_SERVER_PROBLEM,
 };
